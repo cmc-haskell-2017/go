8fb76d69152814f8e11c5bb98df92a42660ac0e0
Mr-Patty <opavlyuchenkov@gmail.com>
2017-03-31 23:53:00 +0300

    fix initBoard

--- src/Game.hs
+++ src/Game.hs
@@ -87,13 +87,13 @@ initGame = Game
 initBoard :: Board
 initBoard  = Map.fromAscList(createList)
 
-createList::[(Point2, Cell)]
+createList:: [(Point2, Cell)]
 createList = createListadd 0 0
 
-createListadd:: Int->Int->[(Point2, Cell)]
+createListadd:: Int -> Int -> [(Point2, Cell)]
 createListadd i1 i2
-  |i2 < boardWidth - 1 = ((i1,i2), Empty) : createListadd i1 (i2+1)
-	|i1 < boardHeight - 1 = ((i1,i2), Empty) : createListadd (i1+1) 0
+  |i2 < boardWidth = ((i1,i2), Empty) : createListadd i1 (i2 + 1)
+  |i1 < boardHeight - 1 = createListadd (i1 + 1) 0
 	|otherwise = []
 
 -- =========================================
@@ -140,12 +140,12 @@ drawStone White = drawWhite
 
 -- | Нарисовать черный камень.
 drawBlack :: Picture
-drawBlack = color black (thickCircle 0 1)
+drawBlack = color black (circleSolid 0.45)
 
 -- | Нарисовать белый камень.
 drawWhite :: Picture
 drawWhite = pictures
-  [color white (thickCircle 0 0.9)
+  [color white (circleSolid 0.45)
   , color black (circle 0.45)
   ]
 
@@ -159,23 +159,29 @@ handleGame :: Event -> Game -> Game
 handleGame (EventKey (MouseButton LeftButton) _ _ mouse) = placeStone (mouseToCell mouse)
 handleGame _ = id
 
+-- | Получить координаты клетки под мышкой.
+mouseToCell :: Point -> Maybe Point2
+mouseToCell (x, y) = (Just (i, j))
+  where
+    i = floor (x + fromIntegral screenWidth  / 2) `div` cellSize
+    j = floor (y + fromIntegral screenHeight / 2) `div` cellSize
 
 -- | Поставить камень и сменить игрока (если возможно).
-placeStone :: Point2 -> Game -> Game -- fix
-placeStone point game =
-  case gameWinner game of
-    Just _ -> game    -- если есть победитель, то поставить фишку нельзя
-    Nothing -> case modifyAt point (gameBoard game) (gamePlayer game) (listBoard game) of --здесь еще нужно дописать функцию преобразования
-      Nothing -> game -- если поставить фишку нельзя, ничего не изменится
-      Just newBoard -> game
-        { gamePlayer = switchPlayer (gamePlayer game)
-        , gameScore = amountScores newBoard
-        , gameComi = gameComi game
-        , gameWinner = winner game
-        , gameBoard  = newBoard
-        , listBoard = (gameBoard game) : (listBoard game)
-        , scoreStones = (0, 0)
-        }
+placeStone :: Maybe Point2 -> Game -> Game -- fix
+placeStone (Just point) game =
+    case gameWinner game of
+      Just _ -> game    -- если есть победитель, то поставить фишку нельзя
+      Nothing -> case modifyAt point (gameBoard game) (gamePlayer game) (listBoard game) of --здесь еще нужно дописать функцию преобразования
+        Nothing -> game -- если поставить фишку нельзя, ничего не изменится
+        Just newBoard -> game
+          { gamePlayer = switchPlayer (gamePlayer game)
+          , gameScore = amountScores newBoard
+          , gameComi = gameComi game
+          , gameWinner = winner game
+          , gameBoard  = newBoard
+          , listBoard = (gameBoard game) : (listBoard game)
+          , scoreStones = (0, 0)
+          }
 
 -- | Применить преобразование к элементу map
 -- с заданным ключом. Если преобразование не удалось — вернуть 'Nothing'.
@@ -243,18 +249,9 @@ switchPlayer White = Black
 amountScores :: Board -> Scores
 amountScores _ = (0.0, 0.0)
 
-
--- | Получить координаты клетки под мышкой.
-mouseToCell :: Point -> Point2
-mouseToCell (x, y) = (i, j)
-  where
-    i = floor (x + fromIntegral screenWidth  / 2) `div` cellSize
-    j = floor (y + fromIntegral screenHeight / 2) `div` cellSize
-
 -- | Определить победителя на игровом поле
 winner :: Game -> Maybe Stone
 winner _ = Nothing
-
 
 -- | Обновление игры.
 -- В этой игре все изменения происходят только по действиям игрока,
@@ -267,14 +264,14 @@ updateGame _ = id
 -- =========================================
 
 -- | Начальная фора(очки) белого игрока
--- надо уточнить отрезок возмодных значений
+-- может варьироваться уже по желания, но это позже в индивидуальных частях
 playerComi :: Float
-playerComi = 5.5
+playerComi = 6.5
 
 -- | начальная фора(камни) черного игрока, надо подумать как это реализовать
 -- обозначает, сколько камней должен поставит черный игрок перед началом партии
 -- это нужно если очковой форы не хватает и игроки слишком разного уровня
--- но не более определенного кол-ва камней, уточним потом в правилах
+-- но не более 9(для доски 19x19), для 9x9 будет не больше 3
 playerHandicap :: Int
 playerHandicap = 0
 
@@ -292,7 +289,7 @@ cellSize = 50
 
 -- | отступы от края экрана
 screenOffset :: Int
-screenOffset = 40
+screenOffset = 50
 
 -- | Ширина экрана в пикселях.
 screenWidth :: Int
