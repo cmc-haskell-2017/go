e058a38be7c31a8a60035d66011675d6230fa148
Mr-Patty <Opavlyuchenkov@gmail.com>
2017-05-04 00:20:52 +0300

    Divided the project into modules

---
+++ src/Draw.hs
@@ -0,0 +1,91 @@
+module Draw where
+
+import Graphics.Gloss.Interface.Pure.Game
+import Config
+import Models
+
+import Data.Char
+import qualified Data.Map as Map
+
+-- =========================================
+-- Отрисовка игры
+-- =========================================
+
+-- | Отрисовка игры, складывает изображения сетки и поля.
+drawGame :: Game -> Picture
+drawGame game = translate (-w) (-h) (scale c c (pictures
+  [ drawGrid
+  , drawBoard (gameBoard game)
+  -- ,drawPass (numberOfPass game)
+  , drawScores (gameScore game)
+  ]))
+  where
+    c = fromIntegral cellSize
+    w = fromIntegral screenWidth  / 2 - offset
+    h = fromIntegral screenHeight / 2 - offset
+    offset = fromIntegral screenOffset / 2
+
+-- | Отрисовка количества пассов
+-- drawPass :: Passes -> Picture
+-- drawPass (x,y) = (scale 0.005 0.005 (text [(intToDigit x), (intToDigit y)]))
+
+-- | Отрисовка количества очков игры
+drawScores :: Scores -> Picture
+drawScores (x, y) = translate w h (scale 0.005 0.005 (text [(intToDigit (round x)), (intToDigit  (round y))] ))
+  where
+    w = fromIntegral screenWidth / 200
+    h = fromIntegral screenHeight / 200
+
+-- | Ортисовка сетки игрового поля.
+drawGrid :: Picture
+drawGrid = color black (pictures (hs ++ vs))
+  where
+    hs = map (\j -> line [(0, j), (n - 1, j)]) [0..m - 1]
+    vs = map (\i -> line [(i, 0), (i, m - 1)]) [0..n - 1]
+
+    n = fromIntegral boardWidth
+    m = fromIntegral boardHeight
+
+-- | Нарисовать фишки на игровом поле.
+drawBoard :: Board -> Picture
+drawBoard board = foldMap drawCells (Map.toList board)
+  where
+    drawCells ((x, y), cell) = translate (fromIntegral x) (fromIntegral y) (drawCell cell)
+
+-- | Нарисовать камень, если он там есть.
+drawCell :: Cell -> Picture
+drawCell Empty = blank
+drawCell (Cell stone) = drawStone stone
+drawCell (CellShadow stone) = drawShadowStone stone
+
+-- | Нарисовать камень.
+drawStone :: Stone -> Picture
+drawStone Black = drawBlack
+drawStone White = drawWhite
+
+-- | Разымытие камня.
+drawShadowStone :: Stone -> Picture
+drawShadowStone Black = drawShadowBlack
+drawShadowStone White = drawShadowWhite
+
+-- | Размытие черного камня.
+drawShadowBlack :: Picture
+drawShadowBlack =  color (withAlpha 0.5 black) (circleSolid radiusStone)
+
+-- | Размытие белого камня.
+drawShadowWhite :: Picture
+drawShadowWhite = pictures
+  [color (withAlpha 0.5 white) (circleSolid radiusStone)
+  , color (withAlpha 0.5 black) (circle radiusStone)
+  ]
+
+-- | Нарисовать черный камень.
+drawBlack :: Picture
+drawBlack = color black (circleSolid radiusStone)
+
+-- | Нарисовать белый камень, с черной каймой.
+drawWhite :: Picture
+drawWhite = pictures
+  [color white (circleSolid radiusStone)
+  , color black (circle radiusStone)
+  ]
---
+++ src/Models.hs
@@ -0,0 +1,85 @@
+module Models where
+
+import Data.Map (Map)
+
+-- =========================================
+-- Модель игры
+-- =========================================
+
+-- | Модель камня игрока.
+data Stone = Black | White
+  deriving(Eq, Show)
+
+-- | Клетка поля.
+data Cell = Cell Stone | CellShadow Stone | Empty
+  deriving(Eq, Show)
+
+-- | Количество очков для одного игрока.
+type Score = Float
+
+-- | Количество очков двух игроков.
+type Scores = (Score, Score)
+
+-- | Наша точка.
+type Node = (Int, Int)
+
+-- | Игровое поле.
+type Board = Map Node Cell
+
+-- | Кол-во камней, которые "съели" черные.
+type Blacksum = Int
+
+-- | Кол-во камней, которые "съели" белые.
+type Whitesum = Int
+
+-- | Кол-во камней убранных каждым игроком.
+type AmountStones = (Blacksum, Whitesum)
+
+-- | Кол-во пассов подряд каждого игрока
+type Passes = (Int, Int)
+
+-- | Состоние поля, мне кажется оно должно быть таким.
+data Game = Game
+  { gamePlayer :: Stone -- чей ход
+  , gameScore :: Scores -- количества очков для первого и второго игрока
+  , gameComi :: Float -- колличество форы
+  , gameWinner :: Maybe Stone -- победитель?!
+  , gameBoard :: Board
+  , listBoard :: [Board] -- список всех предыдущих состояний
+  , scoreStones :: AmountStones -- кол-во камней убранных каждым игроком
+  , numberOfPass :: Passes
+  }
+
+-- =========================================
+-- Инициализация игры
+-- =========================================
+
+-- | Начальное состояние игры.
+-- Игровое поле — пусто.
+-- Первый игрок ходит черными.
+initGame :: Game
+initGame = Game
+  { gamePlayer = Black
+  , gameScore = (0, 0)
+  , gameComi = playerComi
+  , gameWinner = Nothing
+  , gameBoard  = initBoard
+  , listBoard = []
+  , scoreStones = (0, 0)
+  , numberOfPass = (0, 0)
+  }
+
+-- | Построение пустого поля.
+initBoard :: Board
+initBoard  = Map.fromAscList createList
+
+-- | Начало поля с точки (0, 0).
+createList:: [(Node, Cell)]
+createList = createListadd 0 0
+
+-- | Идет рекурсивно по столбцам и строкам и заполняет список пустыми клетками.
+createListadd:: Int -> Int -> [(Node, Cell)]
+createListadd i1 i2
+  |i2 < boardWidth = ((i1, i2), Empty) : createListadd i1 (i2 + 1)
+  |i1 < boardHeight - 1 = createListadd (i1 + 1) 0
+  |otherwise = []
