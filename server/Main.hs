49f1cb3e33684dacea3cc3a2dabf243d9a42ece9
Nikita Taras <nikittara@yandex.ru>
2017-05-30 01:09:07 +0300

    +Server folder

---
+++ server/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Control.Concurrent (forkIO)
+import Network.Wai.Handler.Warp (run)
+import System.Environment
+
+import go.Server
+
+main :: IO ()
+main = do
+  [port] <- getArgs
+  config <- defaultConfig
+  _      <- forkIO $ periodicUpdates 10000 config
+  run (read port :: Int) $ server config
--- server/go/server.hs
+++ server/go/server.hs
@@ -44,3 +44,74 @@ defaultConfig do =
 		<*> newTvar Map.empty
 		<*> newTVar (map show [1..])
 return cfg
+
+-- | API
+type GoAPI = "connect" :> Raw
+
+server :: Config -> Server GoAPI
+server config  = websocketsOr defaultConnectionOptions wsApp backupApp
+where 
+	wsApp :: ServerApp
+	wsApp pending_con = do
+		conn <- AcceptRequest pending_con
+		name <- addClient conn config
+		putStrLn $ name ++ "joined!"
+		hendleActions name conn config
+
+	-- | Используется для не сокетных соединений 
+
+	backupApp _ respond = respond $ responseLBS status400 [] "Not a WebSocket request"
+
+
+-- | Добавление клиента на сервер на сервер
+addClient :: Client -> Config -> IO PlayerName
+addClient client Config{..} = do
+  g <- newStdGen
+  atomically $ do
+    name:names <- readTVar configNames
+    writeTVar configName names
+    modifyTVar configClients (Map.insert name client)
+    return name
+
+-- | Обработка событий с клиентов
+handleActions :: PlayerName -> Connection -> Config -> IO ()
+handleActions name conn cfg@Config{..} = forever $ do
+  action <- receiveData conn
+  atomically $ do
+    modifyTVar configGame (handleGame action)
+
+-- | Обновление доски
+
+periodicUpdates :: Int -> Config -> IO ()
+periodicUpdates ms cfg@Config{..} = forever $ do
+  threadDelay ms -- wait ms milliseconds
+  g <- newStdGen
+  game <- atomically $ do
+    game <- (flip evalRand g . updateSreen secs) <$> readTVar configGame
+    writeTVar configGame game
+    return game
+  broadcastUpdate game cfg
+  where
+    secs = fromIntegral ms / 1000000
+
+-- | Отправка изменений на клиенты
+broadcastUpdate :: Game -> Config -> IO ()
+broadcastUpdate game cfg@Config{..} = do
+  clients <- readTVarIO configClients
+  mapM_ (forkIO . sendUpdate) (Map.toList clients)
+  where
+    sendUpdate (name, conn) = sendBinaryData conn game `catch` handleClosedConnection name
+
+
+-- | Обработка отключения клиента
+ handleClosedConnection :: PlayerName -> ConnectionException -> IO ()
+    handleClosedConnection name _ = do
+      putStrLn (name ++ " disconnected.")
+      atomically $ do
+        modifyTVar configClients  (Map.delete name)
+        modifyTVar configGame (kickPlayer name)
+
+kickPlayer :: PlayerName -> Game -> Game 
+kickPlayer name g = initGame
+
+
